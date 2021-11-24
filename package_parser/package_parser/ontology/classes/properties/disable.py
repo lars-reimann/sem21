854010bd50607bf9ff91a0d501b7e5103df170ac
Aleksandr Sergeev <aleksandr.sergeev@tngtech.com>
2021-11-24 16:19:39 +0100

    2 basic property subclass ideas

--- package_parser/package_parser/ontology/classes/properties/disable.py
+++ package_parser/package_parser/ontology/classes/properties/disable.py
@@ -4,6 +4,6 @@ from package_parser.package_parser.ontology.classes.property import Property
 class Disable(Property):
     """
     The domain Parameter disables the range Parameter.
-    E.g. if domain == "val", range is used.
+    E.g. if domain == "val", range is an ignored parameter.
     """
     pass
---
+++ package_parser/package_parser/ontology/classes/properties/require.py
@@ -0,0 +1,9 @@
+from package_parser.package_parser.ontology.classes.property import Property
+
+
+class InheritsValueFrom(Property):
+    """
+    The domain Parameter inherits its value from an attribute of thethe range Parameter.
+    E.g. if domain == None, attribute of range is used.
+    """
+    pass
