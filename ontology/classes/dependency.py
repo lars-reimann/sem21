c2f7b024febaece57d3db425fc8448358a23581a
Aleksandr Sergeev <aleksandr.sergeev@tngtech.com>
2021-11-28 17:27:06 +0100

    rework ontology model in code

--- ontology/classes/dependency.py
+++
@@ -1,8 +0,0 @@
-from classes.parameter import Parameter
-
-
-class Dependency:
-    def __init__(self, domain: Parameter, range: Parameter, description: str = "No description available." ) -> None:
-        self.range = range
-        self.domain = domain
-        self.description = description
