c6d3d7189a403485586054ec6382cce2c0b6e35f
Prajakta Bhujbal <prajaktabhujbal7@gmail.com>
2021-12-23 12:30:05 +0100

    Added a case of !=

--- package_parser/package_parser/commands/get_dependencies/_preprocess_docstring.py
+++ package_parser/package_parser/commands/get_dependencies/_preprocess_docstring.py
@@ -2,16 +2,23 @@ def preprocess_docstring(docstring: str) -> str:
     """
     1. Remove cluttered punctuation around parameter references
     2. Set '=', ==' to word equals
-    3. Set cases of 'a=b', 'a==b to 'a equals b' (not successful in step 2 due to no spaces)
+    3. Set '!=' to not equals
+    4. Set cases of 'a=b', 'a==b to 'a equals b' (not successful in step 2 due to no spaces)
+
     """
     
-    docstring = docstring.replace('"', '')
+    docstring = docstring.replace('"', '')                  # Case 1
     words = docstring.split()
-    equals_strings = ["=", "=="]
+
+    equals_strings = ["=", "=="]                            # Case 2
+    notEquals_string = ["!="]                               # Case 3
+
     for i, word in enumerate(words):
-        if word in equals_strings:
+        if word in notEquals_string:
+            words[i] = 'not equals'
+        elif word in equals_strings:
             words[i] = "equals"
-        else:
+        else:                                               # Case 4
             # Order here is important! Cleaner way?
             if "==" in word:
                 word = word.replace("==", " equals ")
