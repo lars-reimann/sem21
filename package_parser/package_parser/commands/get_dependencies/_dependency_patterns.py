b1dac89a96b13ad11fccf5e327cff974a32cc7a6
Aleksandr Sergeev <aleksandr.sergeev@tngtech.com>
2021-12-29 15:29:24 +0100

    #35 ignored condition pattern added

--- package_parser/package_parser/commands/get_dependencies/_dependency_patterns.py
+++ package_parser/package_parser/commands/get_dependencies/_dependency_patterns.py
@@ -14,7 +14,25 @@ dependency_matcher_patterns = {
             "LEFT_ID": "condition",
             "REL_OP": ">",
             "RIGHT_ID": "dependee_param",
-            "RIGHT_ATTRS": {"DEP": "nsubj"}
+            "RIGHT_ATTRS": {"DEP": {"IN": ["nsubj", "nsubjpass"]}}
+        }
+    ],
+    "pattern_parameter_ignored_condition": [
+        {
+            "RIGHT_ID": "ignored",
+            "RIGHT_ATTRS": {"ORTH": "ignored"}
+        },
+        {
+            "LEFT_ID": "ignored",
+            "REL_OP": ">",
+            "RIGHT_ID": "condition",
+            "RIGHT_ATTRS": {"DEP": "advcl"}
+        },
+        {
+            "LEFT_ID": "condition",
+            "REL_OP": ">",
+            "RIGHT_ID": "dependee_param",
+            "RIGHT_ATTRS": {"DEP": {"IN": ["nsubj", "nsubjpass"]}}
         }
     ]
 }
