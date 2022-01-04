aa982511f844760dc6412a2ee7ad5eb35d7a189b
Aleksandr Sergeev <aleksandr.sergeev@tngtech.com>
2022-01-04 18:48:53 +0100

    added pattern parameter applies

--- package_parser/package_parser/commands/get_dependencies/_dependency_patterns.py
+++ package_parser/package_parser/commands/get_dependencies/_dependency_patterns.py
@@ -34,5 +34,23 @@ dependency_matcher_patterns = {
             "RIGHT_ID": "dependee_param",
             "RIGHT_ATTRS": {"DEP": {"IN": ["nsubj", "nsubjpass"]}}
         }
+    ],
+    "pattern_parameter_applies_condition": [
+        {
+            "RIGHT_ID": "applies",
+            "RIGHT_ATTRS": {"ORTH": {"IN": ["applies", "Applies"]}}
+        },
+        {
+            "LEFT_ID": "applies",
+            "REL_OP": ">",
+            "RIGHT_ID": "condition",
+            "RIGHT_ATTRS": {"DEP": "advcl"}
+        },
+        {
+            "LEFT_ID": "condition",
+            "REL_OP": ">",
+            "RIGHT_ID": "dependee_param",
+            "RIGHT_ATTRS": {"DEP": {"IN": ["nsubj", "nsubjpass"]}}
+        }
     ]
 }
