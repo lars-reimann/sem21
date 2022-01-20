20bc5f40b9f11c5bf39ebcf8d0559dd32212775b
Dushko Klincharov <klincarov@halicea.com>
2022-01-20 15:09:23 +0100

    Extract boundaries with regex

--- package_parser/package_parser/commands/get_api/_refined_types.py
+++ package_parser/package_parser/commands/get_api/_refined_types.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import re
 from dataclasses import dataclass, field
-from typing import Union
+from typing import Optional, Union
 
 
 @dataclass
@@ -18,16 +18,16 @@ class EnumType:
     values: set[str] = field(default_factory=set)
 
     @classmethod
-    def from_string(cls, string: str) -> EnumType:
+    def from_string(cls, string: str) -> Optional[EnumType]:
         def remove_backslash(e: str):
             e = e.replace(r"\"", '"')
             e = e.replace(r"\'", "'")
             return e
 
         enum_match = re.search(r"{(.*?)}", string)
-        values = set()
-        quotes = "'\""
         if enum_match:
+            quotes = "'\""
+            values = set()
             enum_str = enum_match.group(1)
             value = ""
             inside_value = False
@@ -48,7 +48,9 @@ class EnumType:
                 elif inside_value:
                     value += char
 
-        return EnumType(values)
+            return EnumType(values)
+
+        return None
 
     def update(self, enum: EnumType):
         self.values.update(enum.values)
@@ -57,13 +59,46 @@ class EnumType:
 @dataclass
 class BoundaryType:
     base_type: str
-    min: float
-    max: float
-    min_inclusive: bool = False
-    max_inclusive: bool = False
+    min: Union[float, int]
+    max: Union[float, int]
+    min_inclusive: bool
+    max_inclusive: bool
 
-    def from_string(self, string: str) -> BoundaryType:
-        pass
+    @classmethod
+    def _is_inclusive(cls, bracket: str) -> Optional[bool]:
+        if bracket == "(" or bracket == ")":
+            return False
+        elif bracket == "[" or bracket == "]":
+            return True
+        return None
+
+    @classmethod
+    def from_string(cls, string: str) -> Optional[BoundaryType]:
+        pattern = r"(?P<base_type>float|int)? (?:in|of)(?: the)? (?:range|interval) `?(?P<min_bracket>\[|\()(?P<min>\d+.?\d*), (?P<max>\d+.?\d*|infinity)(?P<max_bracket>\]|\))`?"
+        match = re.search(pattern, string)
+
+        if match is not None:
+            base_type = match.group("base_type")
+            if base_type is None:
+                base_type = "float"
+            base_type = eval(base_type)
+
+            min_value = base_type(match.group("min"))
+            max_value = match.group("max")
+            if max_value != "infinity":
+                max_value = base_type(max_value)
+            else:
+                max_value = float("inf")
+            min_bracket = match.group("min_bracket")
+            max_bracket = match.group("max_bracket")
+            min_inclusive = BoundaryType._is_inclusive(min_bracket)
+            max_inclusive = BoundaryType._is_inclusive(max_bracket)
+
+            return BoundaryType(
+                base_type, min_value, max_value, min_inclusive, max_inclusive
+            )
+
+        return None
 
 
 @dataclass
